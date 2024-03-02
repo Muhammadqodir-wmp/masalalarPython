69fd2b13bb88ce8392ba5bab70f7327b7efc527d
Muhammadqodir <136580277+BAJARUVCHI@users.noreply.github.com>
2024-03-02 21:20:16 +0500

    kabisa yili masalasi

---
+++ masala0001.py
@@ -0,0 +1,16 @@
+def is_leap(yil):
+    if yil % 4 == 0:
+        if yil % 100 == 0 and  yil % 400 == 0 :           
+            return True
+        elif yil % 4 == 0 and yil % 100!=0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+year = int(input())
+print(is_leap(year))
+
+
